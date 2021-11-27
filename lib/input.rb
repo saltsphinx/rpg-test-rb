e01dbf9ba8d9bda92c76ac5270389b2a0c79cf31
saltsphinx <tremar.thomas19@gmail.com>
2021-11-26 20:06:40 -0600

    Add to #get_input lib/input

--- lib/input.rb
+++ lib/input.rb
@@ -1,6 +1,10 @@
 # frozen-string-literal: true
 
+require_relative './cmd'
+
 class Input
+  include CMD
+
   def initalize
     @options = ['table1', 'table2']
   end
@@ -9,6 +13,7 @@ class Input
     user_input = gets
     user_input = user_input.chomp unless user_input.nil?
     parsed_input = parse_input(user_input)
+    command = CMDS[parsed_input.first]
   end
 
   def parse_input(input)

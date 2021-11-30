f4b21d039aba262c4d5ed701523790f290da1bf1
saltsphinx <tremar.thomas19@gmail.com>
2021-11-30 02:48:10 -0600

    Add items to table

--- lib/game.rb
+++ lib/game.rb
@@ -28,10 +28,11 @@ class Game
   def new_game
     @player = Player.new
     apple = Item.new('apple', 'A bright red apple with a brown stem and a pretty green leaf atop.')
+    apple2 = Item.new('apple', 'A bright red apple with a brown stem and a pretty green leaf atop.')
     spoon = Container.new([], 'spoon', 'A silver spoon.')
     bowl = Container.new([apple, spoon], 'bowl', 'A wide brimmed, wooden bowl.')
     cup = Container.new([], 'cup', 'A small wooden cup.')
-    table = ContainerObject.new([bowl, cup], 'table', 'A large, old, oval wooden table, made of dark oak.')
+    table = ContainerObject.new([bowl, cup, apple2], 'table', 'A large, old, oval wooden table, made of dark oak.')
     @room = Room.new([table], 'Starter room')
   end
 
